9dbc050954e6b965e37ed69b8549a6116d6bb5b3
ACER TRAVELMATE <ACER TRAVELMATE@DSWD>
2023-05-24 15:51:29 +0800

    USER

---
+++ SWEAP_v1/userdashboard.Designer.vb
@@ -0,0 +1,305 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class userdashboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Panel2 = New Panel()
+        Button4 = New Button()
+        Button3 = New Button()
+        Button2 = New Button()
+        position = New Label()
+        name = New Label()
+        PictureBox1 = New PictureBox()
+        Button1 = New Button()
+        Panel1 = New Panel()
+        Panel3 = New Panel()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Panel4 = New Panel()
+        Panel5 = New Panel()
+        Label3 = New Label()
+        TextBox1 = New TextBox()
+        DataGridView1 = New DataGridView()
+        Panel2.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel3.SuspendLayout()
+        Panel4.SuspendLayout()
+        Panel5.SuspendLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.FromArgb(CByte(255), CByte(77), CByte(77))
+        Panel2.Controls.Add(Button4)
+        Panel2.Controls.Add(Button3)
+        Panel2.Controls.Add(Button2)
+        Panel2.Controls.Add(position)
+        Panel2.Controls.Add(name)
+        Panel2.Controls.Add(PictureBox1)
+        Panel2.Dock = DockStyle.Left
+        Panel2.Location = New Point(0, 39)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(245, 751)
+        Panel2.TabIndex = 1
+        ' 
+        ' Button4
+        ' 
+        Button4.Cursor = Cursors.Hand
+        Button4.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button4.FlatStyle = FlatStyle.Flat
+        Button4.ForeColor = SystemColors.ButtonHighlight
+        Button4.Location = New Point(3, 245)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(239, 35)
+        Button4.TabIndex = 5
+        Button4.Text = "DASHBOARD"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Cursor = Cursors.Hand
+        Button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button3.FlatStyle = FlatStyle.Flat
+        Button3.ForeColor = SystemColors.ButtonHighlight
+        Button3.Location = New Point(3, 327)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(239, 35)
+        Button3.TabIndex = 4
+        Button3.Text = "VIEW MEMBER"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Cursor = Cursors.Hand
+        Button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.ForeColor = SystemColors.ButtonHighlight
+        Button2.Location = New Point(3, 286)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(239, 35)
+        Button2.TabIndex = 3
+        Button2.Text = "PROFILE"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' position
+        ' 
+        position.Dock = DockStyle.Top
+        position.Font = New Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point)
+        position.ForeColor = SystemColors.ButtonFace
+        position.Location = New Point(0, 190)
+        position.Name = "position"
+        position.Size = New Size(245, 20)
+        position.TabIndex = 2
+        position.Text = "OJT"
+        position.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' name
+        ' 
+        name.Dock = DockStyle.Top
+        name.Font = New Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        name.ForeColor = SystemColors.ButtonFace
+        name.Location = New Point(0, 164)
+        name.Name = "name"
+        name.Size = New Size(245, 26)
+        name.TabIndex = 1
+        name.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Dock = DockStyle.Top
+        PictureBox1.Location = New Point(0, 0)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(245, 164)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.ForeColor = SystemColors.ButtonHighlight
+        Button1.Location = New Point(1349, 0)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(47, 36)
+        Button1.TabIndex = 2
+        Button1.Text = "X"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        Panel1.Location = New Point(270, 64)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1101, 182)
+        Panel1.TabIndex = 3
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.Silver
+        Panel3.Controls.Add(Label7)
+        Panel3.Controls.Add(Label6)
+        Panel3.Controls.Add(Label5)
+        Panel3.Controls.Add(Label4)
+        Panel3.Location = New Point(252, 42)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(1136, 742)
+        Panel3.TabIndex = 4
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI Semibold", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.ForeColor = SystemColors.ActiveCaptionText
+        Label7.Location = New Point(21, 283)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(57, 21)
+        Label7.TabIndex = 6
+        Label7.Text = "Name:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Semibold", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.ForeColor = SystemColors.ActiveCaptionText
+        Label6.Location = New Point(21, 247)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(57, 21)
+        Label6.TabIndex = 5
+        Label6.Text = "Name:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Semibold", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.ForeColor = SystemColors.ActiveCaptionText
+        Label5.Location = New Point(21, 207)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(57, 21)
+        Label5.TabIndex = 4
+        Label5.Text = "Name:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Semibold", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.ForeColor = SystemColors.ActiveCaptionText
+        Label4.Location = New Point(21, 161)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(57, 21)
+        Label4.TabIndex = 3
+        Label4.Text = "Name:"
+        ' 
+        ' Panel4
+        ' 
+        Panel4.Controls.Add(Button1)
+        Panel4.Dock = DockStyle.Top
+        Panel4.Location = New Point(0, 0)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(1396, 39)
+        Panel4.TabIndex = 5
+        ' 
+        ' Panel5
+        ' 
+        Panel5.BackColor = Color.Teal
+        Panel5.Controls.Add(Label3)
+        Panel5.Controls.Add(TextBox1)
+        Panel5.Controls.Add(DataGridView1)
+        Panel5.Location = New Point(251, 42)
+        Panel5.Name = "Panel5"
+        Panel5.Size = New Size(1136, 742)
+        Panel5.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Semibold", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.ForeColor = SystemColors.ButtonFace
+        Label3.Location = New Point(828, 14)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(63, 21)
+        Label3.TabIndex = 2
+        Label3.Text = "Search:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(895, 13)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(237, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(4, 51)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(1129, 685)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' userdashboard
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(56), CByte(103), CByte(214))
+        ClientSize = New Size(1396, 790)
+        Controls.Add(Panel2)
+        Controls.Add(Panel4)
+        Controls.Add(Panel3)
+        Controls.Add(Panel1)
+        Controls.Add(Panel5)
+        FormBorderStyle = FormBorderStyle.None
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "userdashboard"
+        Panel2.ResumeLayout(False)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        Panel4.ResumeLayout(False)
+        Panel5.ResumeLayout(False)
+        Panel5.PerformLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents position As Label
+    Friend WithEvents name As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ SWEAP_v1/userdashboard.vb
@@ -0,0 +1,49 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class userdashboard
+    Dim co As String = "server=localhost;port=3306;user=root;password=;database=sweap"
+    Dim conn As New MySqlConnection(co)
+    Dim cmd As MySqlCommand
+    Dim i As Integer
+    Dim dr As MySqlDataReader
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Panel3.Show()
+        Panel1.Hide()
+        Panel5.Hide()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Panel3.Hide()
+        Panel1.Show()
+        Panel5.Hide()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Panel5.Show()
+        Panel1.Hide()
+        Panel3.Hide()
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles name.Click
+        conn.Open()
+
+        Dim query As String = "SELECT name FROM register WHERE id = @id"
+
+        Dim cmd As New MySqlCommand(query, conn)
+        cmd.Parameters.AddWithValue("@id", )
+        Dim reader As MySqlDataReader = cmd.ExecuteReader()
+
+        If reader.Read() Then
+            name.Text = reader("name").ToString()
+        End If
+
+        reader.Close()
+        conn.Close()
+
+    End Sub
+End Class
